b13eda3b75efb9a99fd524a8c8662baae49828bb
Ronaldo Vieira <ronaldoesv@gmail.com>
2017-01-26 15:59:48 +0000

    Updates sequential version

--- src/sequential/log-edges.cc
+++ src/sequential/log-edges.cc
@@ -2,7 +2,7 @@
  ============================================================================
  Name        : log-edges.cc
  Author      : Ronaldo Vieira
- Version     : 1.0.0
+ Version     : 0.0.1
  Copyright   : MIT License
  Description : Edge detection in images using a parallel and distributed 
 laplacian-of-gaussian filter.
@@ -10,6 +10,7 @@ laplacian-of-gaussian filter.
 */
 #include <iostream>
 #include <string>
+#include <cstring>
 #include <cstdio>
 #include <ctime>
 #include <cmath>
@@ -22,7 +23,10 @@ laplacian-of-gaussian filter.
 
 using std::cout;
 using std::endl;
+using std::min;
+using std::max;
 using std::string;
+using std::memcpy;
 
 static long get_nanos(void) {
     struct timespec ts;
@@ -30,26 +34,29 @@ static long get_nanos(void) {
     return (long) ts.tv_sec * 1000000000L + ts.tv_nsec;
 }
 
-bool isInBounds(int x, int y, int w, int h) {
-	return x >= 0 && x < w  && y >= 0 && y < h;
-}
-
-void applyFilter(pixel **inMat, pixel **outMat, int w, int h) {
-	int sum, amount;
+int* applyFilter(int *mat, int w, int h) {
+	int sum, amount, tempX, tempY;
+	
+	int *orig = (int*) malloc(sizeof(int) * w * h);
+	memcpy(orig, mat, sizeof(int) * w * h);
 	
 	/* for each pixel in the image */
-    for (int y = 0; y < h; ++y) {
-        for (int x = 0; x < w; ++x) {
+    for (int x = 0; x < w; ++x) {
+        for (int y = 0; y < h; ++y) {
             sum = 0;
             amount = 0;
             
             /* apply the kernel matrix */
             for (int j = 0; j < 5; ++j) {
                 for (int i = 0; i < 5; ++i) {
-                    if (isInBounds(x + i - 2, y + j - 2, w, h)) {
-                        sum += lapOfGau[i][j] * inMat[y + j - 2][x + i - 2].value;
-                        amount += lapOfGau[i][j];
-                    }
+                	tempX = x + i - ((int) (5 / 2));
+                	tempY = y + j - ((int) (5 / 2));
+                	
+                	tempX = min(max(tempX, 0), w - 1);
+                	tempY = min(max(tempY, 0), h - 1);
+                	
+                    sum += lapOfGau[i][j] * orig[tempX + tempY * w];
+                    amount += lapOfGau[i][j];
                 }
             }
             
@@ -58,25 +65,33 @@ void applyFilter(pixel **inMat, pixel **outMat, int w, int h) {
             if (sum > 255) sum = 255;
             if (sum < 0) sum = 0;
             
-            outMat[y][x].value = sum;
+            mat[x + y * w] = sum;
         }
     }
+    
+    free(orig);
 }
 
 int main(int argc, char* argv[]) {
 	PixelLab *inImg = new PixelLab(); /* input image */
 	PixelLab *outImg = new PixelLab(); /* output image */
 	
-	pixel **inMat, **outMat;
+	int *outMat;
 	
-	long start_t, end_t, total_t; /* time measure */
+	double start_t, end_t, total_t; /* time measure */
+	
+	int origWidth, origHeight, /* original image size */
+		width, height, /* slice size */
+		*mat;
 
+	/* validates arguments */
 	if (argc != 2) {
 		cout << "Usage: " << argv[0] << " (image path)" << endl;
 
 		return -1;
 	}
 
+	/* pre processing */
 	string inImgPath = argv[1];
 	FILE *fp = fopen(inImgPath.c_str(), "rb");
 
@@ -91,32 +106,37 @@ int main(int argc, char* argv[]) {
 	inImg->Read(inImgPath.c_str());
 	outImg->Copy(inImg);
 	
-	inImg->AllocatePixelMatrix(&inMat, inImg->GetHeight(), inImg->GetWidth());
-	inImg->GetDataAsMatrix(inMat);
-	outImg->AllocatePixelMatrix(&outMat, outImg->GetHeight(), outImg->GetWidth());
-	outImg->GetDataAsMatrix(outMat);
+	origWidth = inImg->GetWidth();
+	origHeight = inImg->GetHeight();
+	
+	width = origWidth;
+	height = origHeight;
 	
 	/* starts timer */
-	start_t = get_nanos();
+	start_t = MPI_Wtime();
+	
+	outMat = (int*) malloc(sizeof(int) * origWidth * origWidth);
+		
+	for (int y = 0; y < origHeight; y++) {
+		for (int x = 0; x < origWidth; x++) {
+			outMat[x + y * origWidth] = inImg->GetGrayValue(x, y);
+		}
+	}
 	
 	/* applies filter */
-	applyFilter(inMat, outMat, inImg->GetWidth(), inImg->GetHeight());
-    
-    /* finishes timer */
-    end_t = get_nanos();
-    
-    total_t = end_t - start_t;
-    cout << "Time elapsed: " << total_t << "ns" << endl;
+	applyFilter(outMat, width, height);
+		
+	// finishes timer
+	end_t = MPI_Wtime();
 	
-	outImg->SetDataAsMatrix(outMat);
-	
-	inImg->DeallocatePixelMatrix(&inMat, inImg->GetHeight(), inImg->GetWidth());
-	outImg->DeallocatePixelMatrix(&outMat, outImg->GetHeight(), outImg->GetWidth());
+	total_t = end_t - start_t;
+	cout << "Time elapsed: " << total_t << "s" << endl;
 	
 	outImg->Save("examples/lenaGrayOut.png");
 	
 	free(inImg);
 	free(outImg);
+	free(outMat);
 	
 	return 0;
 }
